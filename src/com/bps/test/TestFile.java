4b4620b3ae71459f3394a870d6efd5b4b7fc1775
bsaradhy <biju_vazhapally@yahoo.co.in>
2019-10-29 17:13:45 +0530

    committing changes

--- src/com/bps/test/TestFile.java
+++ src/com/bps/test/TestFile.java
@@ -5,6 +5,7 @@ public class TestFile {
 	public static void main(String[] args) {
 		System.out.println("First GIT repository");
 		System.out.println("First GIT repository - Change 1");
+		System.out.println("First GIT repository - Change 2");
 	}
 
 }
--- src/com/bps/test/TestFile2.java
+++ src/com/bps/test/TestFile2.java
@@ -2,4 +2,7 @@ package com.bps.test;
 
 public class TestFile2 {
 
+	public void testMethod() {
+		System.out.println("TestFile2");
+	}
 }
